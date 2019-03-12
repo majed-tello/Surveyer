b3c5af3a015a09f78c13703482e13793c9c0c270
majed tello <majed.tello97@gmail.com>
2019-03-12 15:34:17 +0200

    first commit

---
+++ Surveyer/Surveyer/Controllers/UsersManagementController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Surveyer.Controllers
+{
+    public class UsersManagementController : Controller
+    {
+        // GET: UsersManagement
+        public ActionResult Loggin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Loggin(string UserName,string Password)
+        {
+            return RedirectToAction("");
+        }
+
+        public ActionResult Loggoff()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(string UserName, string Password)
+        {
+            return RedirectToAction("");
+        }
+    }
+}
---
+++ Surveyer/Surveyer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Surveyer.MvcApplication" Language="C#" %>
