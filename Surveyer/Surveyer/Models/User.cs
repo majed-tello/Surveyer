33a4e33375fa129def97a44b3258fc992ad20131
majed tello <majed.tello97@gmail.com>
2019-03-29 16:05:47 +0300

    add image to user

--- Surveyer/Surveyer/Models/User.cs
+++ Surveyer/Surveyer/Models/User.cs
@@ -13,24 +13,33 @@ namespace Surveyer.Models
         [ScaffoldColumn(false)]
         public string Id { get; set; }
 
+        [Display(Name ="User name")]
         [Required]
         public string UserName { get; set; }
 
+        [Display(Name = "Birth date")]
         [Required]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
+        [Display(Name = "Phone")]
+        [DataType(DataType.PhoneNumber)]
         [Required]
         [Phone]
         public string Phone { get; set; }
 
+        [Display(Name = "Email")]
         [Required]
         [EmailAddress]
         public string Email { get; set; }
 
+        [Display(Name = "Password")]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = "User image")]
+        public string ImageURL { get; set; }
 
         public User()
         {
